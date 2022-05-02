c32fbdd4ad929110dcb26f34a72678a9346d56b8
Salman Wahed <dev.salmanwahed@gmail.com>
2022-05-03 02:12:57 +0600

    Template Updated

--- salmanwahed_com/blog/views.py
+++ salmanwahed_com/blog/views.py
@@ -7,16 +7,16 @@ from django.shortcuts import get_object_or_404
 
 def homepage(request):
     blogs = BlogPost.objects.filter(status=BlogPost.Status.PUBLISHED).order_by('-created_at')
-    return render(request, 'blog/homepage.html', dict(blogs=blogs))
+    return render(request, 'blog/blog_list.html', dict(blogs=blogs))
 
 
 def post_detail(request, id, slug=None):
     blog = get_object_or_404(BlogPost, pk=id)
     blog.visited_count += 1
     blog.save()
-    return render(request, 'blog/detail.html', dict(blog=blog))
+    return render(request, 'blog/blog.html', dict(blog=blog))
 
 
 def tagged_posts(request, tag):
     blogs = BlogPost.objects.filter(tag__tag_name=tag)
-    return render(request, 'blog/homepage.html', dict(blogs=blogs))
+    return render(request, 'blog/blog_list.html', dict(blogs=blogs))
