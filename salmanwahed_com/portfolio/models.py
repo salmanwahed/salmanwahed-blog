3b413d3d9e2227ca813c2fb980229afe989bcfeb
Salman Wahed <dev.salmanwahed@gmail.com>
2022-06-08 21:36:59 +0600

    - Admin site url root changed.

--- salmanwahed_com/portfolio/models.py
+++ salmanwahed_com/portfolio/models.py
@@ -67,3 +67,6 @@ class Project(models.Model):
     utm_url = models.URLField(verbose_name='UTM Url', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- salmanwahed_com/salmanwahed_com/urls.py
+++ salmanwahed_com/salmanwahed_com/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
-    path('admin/', admin.site.urls),
+    path('dehawnamlas/', admin.site.urls),
     path('', include('blog.urls')),
     path('portfolio/', include('portfolio.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
