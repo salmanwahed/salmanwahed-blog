f780fb1df28cd307af90ab4d06dfa50e99d582a3
Salman Wahed <dev.salmanwahed@gmail.com>
2022-06-10 17:32:33 +0600

    - Updated staticfiles directory

--- salmanwahed_com/salmanwahed_com/settings.py
+++ salmanwahed_com/salmanwahed_com/settings.py
@@ -179,7 +179,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR.joinpath('static')
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
