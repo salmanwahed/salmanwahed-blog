045c1ab2401a2ca6298191da98f1102c92fadf4c
Salman Wahed <dev.salmanwahed@gmail.com>
2022-06-10 16:04:53 +0600

    - Set debug false

--- salmanwahed_com/salmanwahed_com/settings.py
+++ salmanwahed_com/salmanwahed_com/settings.py
@@ -29,7 +29,7 @@ else:
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 LOG_DIR = BASE_DIR.joinpath('log')
 LOG_FILE = 'salmanwahed_com.log'
