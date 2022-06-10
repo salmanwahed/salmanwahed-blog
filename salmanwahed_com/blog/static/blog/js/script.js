29f44011dfb8731c7a2c05bddb9c051c41920a4e
Salman Wahed <dev.salmanwahed@gmail.com>
2022-06-10 17:34:15 +0600

    - Added static files folder

---
+++ salmanwahed_com/blog/static/blog/js/script.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+    $(".post-body").find('img').parent().addClass("d-flex justify-content-center")
+    $(".post-body").find('table').addClass("table table-hover")
+});
