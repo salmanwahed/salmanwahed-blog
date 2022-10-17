c90452058a7552cd761f1c58b3bc00fb4ef1d43d
Salman Wahed <dev.salmanwahed@gmail.com>
2022-10-18 01:19:08 +0600

    - Updated minutes to read and pub date for preview

--- salmanwahed_com/blog/templatetags/blog_extras.py
+++ salmanwahed_com/blog/templatetags/blog_extras.py
@@ -9,4 +9,8 @@ register = template.Library()
 def minutes_to_read(body):
     soup = BeautifulSoup(body, features="html.parser")
     words = soup.text.split()
-    return ceil(len(words)/150)
+    minutes_read = ceil(len(words)/150)
+    if minutes_read > 1:
+        return f"{minutes_read} minutes read"
+    else:
+        return f"{minutes_read} minute read"
